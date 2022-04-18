b246236751d941631a24f5d5b1256d13c035fa98
laziza@mail.ru <laziza00>
2022-04-18 11:50:12 +0500

    initial commit

--- js/object.js
+++ js/object.js
@@ -1,2 +1,24 @@
 
+let btnid = document.querySelector('.btnid');
+let box = document.querySelector('.demo')
+btnid.addEventListener('click', ()=> {
+    let outRequest = new XMLHttpRequest();
+    
+    outRequest.open(
+        "Get", "https://learnwebcode.github.io/json-example/animals-1.json"
+    );
 
+    outRequest.onload = function() {
+        let outData = JSON.parse(outRequest.responseText);
+        renderHtml(outData)
+    };
+    outRequest.send()
+})
+function renderHtml(obj) {
+
+    stringData = ""
+    obj.forEach(element => {
+        stringData += "<p>" +  element.name  + " is a " + element.species + "</p> " ;
+        box.innerHTML =stringData
+    });
+}
